67a890afcd1b3e464e4ba413c2defd965de39628
Scott Gebert <scott22.gebert@gmail.com>
2023-04-16 23:49:46 -0600

    Init

---
+++ beforeitwascool/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+
+const routes: Routes = [
+  { path: 'welcome-page', component: WelcomePageComponent },
+  { path: '', component: WelcomePageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
